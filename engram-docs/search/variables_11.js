d4f49dfaebb56811f66bfffc4d2a9c8ce2ed2296
naliferopoulos <aliferopoulos@icloud.com>
2026-06-09 18:55:50 +0000

    Deploying to main from @ Blister-Studios/Engram@3cf2998870a80d4363f2b6a6ecf6f87bf665f09d 🚀

--- engram-docs/search/variables_11.js
+++ engram-docs/search/variables_11.js
@@ -8,11 +8,12 @@ var searchData=
   ['selected_5fgo_5',['selected_go',['../structEditorState.html#aaf288a00c4a91dca072287e209c6cfa6',1,'EditorState']]],
   ['selected_5fso_6',['selected_so',['../structEditorState.html#a04669d70ab23a23687b5ce074cf43bbb',1,'EditorState']]],
   ['set_5flighting_7',['set_lighting',['../structRenderBackend.html#ab11e9dd15eccd75d62c1d4004786afb0',1,'RenderBackend']]],
-  ['shutdown_8',['shutdown',['../structRenderBackend.html#a9102a96fb3ff78e3e274176d5678f136',1,'RenderBackend']]],
-  ['size_9',['size',['../structCamera.html#adaa2ba07777e210a38835f66ea97cc4c',1,'Camera::size'],['../structCollider.html#ab125d00ef3ed6de77aa446a4de77862e',1,'Collider::size']]],
-  ['spot_5finner_5fangle_10',['spot_inner_angle',['../structLight.html#a048482eb365e6216ccd4724cedaef763',1,'Light']]],
-  ['spot_5fouter_5fangle_11',['spot_outer_angle',['../structLight.html#a48f0e5529f387ee8fc65fed3bb9473ee',1,'Light']]],
-  ['sub_5fmesh_5fcount_12',['sub_mesh_count',['../structMeshResource.html#a7ddbcce21b5460349c12195929dd05e1',1,'MeshResource']]],
-  ['sub_5fmeshes_13',['sub_meshes',['../structMeshResource.html#afdaeb396f78b793dbab0ce3cd48270ac',1,'MeshResource']]],
-  ['submit_14',['submit',['../structRenderBackend.html#a3501bcc8319c521aae468dddd2f3f386',1,'RenderBackend']]]
+  ['set_5fshadow_5fdata_8',['set_shadow_data',['../structRenderBackend.html#ac6243f3d0c9f6a851f18133d62dd1d1f',1,'RenderBackend']]],
+  ['shutdown_9',['shutdown',['../structRenderBackend.html#a9102a96fb3ff78e3e274176d5678f136',1,'RenderBackend']]],
+  ['size_10',['size',['../structCamera.html#adaa2ba07777e210a38835f66ea97cc4c',1,'Camera::size'],['../structCollider.html#ab125d00ef3ed6de77aa446a4de77862e',1,'Collider::size']]],
+  ['spot_5finner_5fangle_11',['spot_inner_angle',['../structLight.html#a048482eb365e6216ccd4724cedaef763',1,'Light']]],
+  ['spot_5fouter_5fangle_12',['spot_outer_angle',['../structLight.html#a48f0e5529f387ee8fc65fed3bb9473ee',1,'Light']]],
+  ['sub_5fmesh_5fcount_13',['sub_mesh_count',['../structMeshResource.html#a7ddbcce21b5460349c12195929dd05e1',1,'MeshResource']]],
+  ['sub_5fmeshes_14',['sub_meshes',['../structMeshResource.html#afdaeb396f78b793dbab0ce3cd48270ac',1,'MeshResource']]],
+  ['submit_15',['submit',['../structRenderBackend.html#a3501bcc8319c521aae468dddd2f3f386',1,'RenderBackend']]]
 ];
